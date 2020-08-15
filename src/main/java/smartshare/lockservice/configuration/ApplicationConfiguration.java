812353d0584c2f479cab35de1dccb50bfa64f019
Sethuram Ramalinga Reddy <ramalins@tcd.ie>
2020-08-15 16:25:25 +0100

    implemented instrumentation

--- src/main/java/smartshare/lockservice/configuration/ApplicationConfiguration.java
+++ src/main/java/smartshare/lockservice/configuration/ApplicationConfiguration.java
@@ -2,6 +2,8 @@ package smartshare.lockservice.configuration;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,4 +14,11 @@ public class ApplicationConfiguration {
     public ObjectMapper customObjectMapperForKafka() {
         return new ObjectMapper();
     }
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+        return registry -> registry.config().commonTags( "app", "Lock Service" );
+    }
+
+
 }
